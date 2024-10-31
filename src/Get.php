ab7aaf0b8bb03ef42378b2a5923e08cc459bc9a6
ponponumi <ponponumi@gmail.com>
2024-11-01 01:35:15 +0900

    数値からアルファベットを生成するためのメソッドを作成

--- src/Get.php
+++ src/Get.php
@@ -4,5 +4,25 @@ namespace Ponponumi\PermissionCheck;
 
 class Get
 {
-    //
+    public array $numToalphabetData = [
+        "---",
+        "--x",
+        "-w-",
+        "-wx",
+        "r--",
+        "r-x",
+        "rw-",
+        "rwx",
+    ];
+
+    public function numToAlphabet($num)
+    {
+        $num = intval($num);
+
+        if($num >= 1 && $num <= 7){
+            return $this->numToalphabetData[$num];
+        }else{
+            return $this->numToalphabetData[0];
+        }
+    }
 }
