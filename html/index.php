9568e31a57cfd6eaaa4f382b6a8b7899cb774a42
ponponumi <ponponumi@gmail.com>
2024-11-01 02:21:14 +0900

    自分の情報を取得するテストを追加

--- html/index.php
+++ html/index.php
@@ -12,3 +12,4 @@ for ($i = -1; $i <= 8; $i++){
 
 var_dump($get->dataGet(__FILE__));                  // ファイルがある
 var_dump($get->dataGet(__DIR__ . '/hello.txt'));    // ファイルがない
+var_dump($get->myGet());                            // 自分の情報を取得
