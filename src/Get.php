197ce1b6f6f65b45901ae46d25435b0d17c52925
ponponumi <ponponumi@gmail.com>
2024-11-01 01:47:58 +0900

    ファイルまたはフォルダからパーミッションを検索するメソッドを作成

--- src/Get.php
+++ src/Get.php
@@ -37,4 +37,30 @@ class Get
 
         return $default;
     }
+
+    public function filePermissionGet(string $path,$alphabetMode=false)
+    {
+        // ファイルまたはフォルダからパーミッションを検索する
+        // ファイルまたはフォルダがない場合は空文字を返す
+        $result = "";
+
+        if(file_exists($path)){
+            // ファイルまたはフォルダがあれば
+            $perms = fileperms($path);
+            $permOct = sprintf('%o',$perms);
+            $result = substr($permOct,-3);
+
+            if($alphabetMode){
+                // アルファベットモードが有効であれば
+                $list = str_split($result);
+                $result = "";
+
+                foreach($list as $item){
+                    $result .= self::numToAlphabet($item);
+                }
+            }
+        }
+
+        return $result;
+    }
 }
